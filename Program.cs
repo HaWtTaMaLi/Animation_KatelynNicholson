f6b7a289f47ec21fb373a3c498dc14807b0c555c
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-02 17:28:02 -0300

    Done.
    i tried really hard to get that second dot lol

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,10 +18,10 @@ namespace Animation_KatelynNicholson
             Console.SetCursorPosition(10, 5);
             Console.Write("  ");
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++) //right 10
             {
                 Console.BackgroundColor = ConsoleColor.Blue;
-                Console.SetCursorPosition(i, 0);
+                Console.SetCursorPosition(i, 0);// (Verticle, Horizontal)
                 Console.Write("   ");
 
                 if (i > 0)
@@ -33,7 +34,7 @@ namespace Animation_KatelynNicholson
                 Thread.Sleep(100);
             }
 
-            for (int i = 0; i < 10; i++) //numbers X, Y 
+            for (int i = 0; i < 6; i++) //DOWN 6
             {
                 Console.BackgroundColor = ConsoleColor.Blue;
                 Console.SetCursorPosition(10, i);
@@ -53,22 +54,37 @@ namespace Animation_KatelynNicholson
             Console.SetCursorPosition(25, 15);
             Console.Write("  ");
 
-            for (int i = 5; i < 5; i++) //numbers X, Y 
+            for (int i = 6; i < 16; i++) //Down 6
             {
                 Console.BackgroundColor = ConsoleColor.Blue;
-                Console.SetCursorPosition(5, i);
+                Console.SetCursorPosition(10, i);
                 Console.Write("  ");
 
                 if (i > 0)
                 {
                     Console.BackgroundColor = ConsoleColor.Black;
                     Console.SetCursorPosition(10, i - 1);
-                    Console.Write("  ");
+                    Console.Write("   ");
                 }
 
                 Thread.Sleep(100);
             }
 
+            for (int i = 10; i < 25; i++) //Right
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(i, 15);
+                Console.Write("   ");
+
+                if (i > 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.SetCursorPosition(i - 1, 15);
+                    Console.Write("  ");
+                }
+
+                Thread.Sleep(100);
+            }
 
             Console.ReadKey();
         }
